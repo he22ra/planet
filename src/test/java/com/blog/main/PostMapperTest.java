2d244d8e0023fb56949a04405c1775d470ab897e
he22ra <hhraleeee@gamil.com>
2023-05-18 16:46:11 +0900

    게시판 테스트 : delete 완료

--- src/test/java/com/blog/main/PostMapperTest.java
+++ src/test/java/com/blog/main/PostMapperTest.java
@@ -46,26 +46,35 @@ public class PostMapperTest {
 //	        }
 //	    }
 		
-		@Test
-	    void update() {
-	        // 1. 게시글 수정
-	        PostRequest params = new PostRequest();
-	        params.setId(1L);
-	        params.setTitle("1번 게시글 제목 수정합니다.");
-	        params.setContents("1번 게시글 내용 수정합니다.");
-	        params.setWriter("수정테스터");
-	        params.setNoticeYn(1);
-	        postMapper.update(params);
-
-	        // 2. 게시글 상세정보 조회
-	        PostResponse post = postMapper.findById(1L);
-	        try {
-	            String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
-	            System.out.println(postJson);
-
-	        } catch (JsonProcessingException e) {
-	            throw new RuntimeException(e);
-	        }
+//		@Test
+//	    void update() {
+//	        // 1. 게시글 수정
+//	        PostRequest params = new PostRequest();
+//	        params.setId(1L);
+//	        params.setTitle("1번 게시글 제목 수정합니다.");
+//	        params.setContents("1번 게시글 내용 수정합니다.");
+//	        params.setWriter("수정테스터");
+//	        params.setNoticeYn(1);
+//	        postMapper.update(params);
+//
+//	        // 2. 게시글 상세정보 조회
+//	        PostResponse post = postMapper.findById(1L);
+//	        try {
+//	            String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
+//	            System.out.println(postJson);
+//
+//	        } catch (JsonProcessingException e) {
+//	            throw new RuntimeException(e);
+//	        }
+//	    }
+		
+		// 삭제 여부(delete_yn) 상태 값을 0(false)에서 1(true)로 UPDATE
+		// 추후에 게시글 리스트 페이지에는 delete_yn이 0(false)인 데이터만 사용자에게 노출
+	    @Test
+	    void delete() {
+	        System.out.println("삭제 이전의 전체 게시글 개수는 : " + postMapper.findAll().size() + "개입니다.");
+	        postMapper.deleteById(1L);
+	        System.out.println("삭제 이후의 전체 게시글 개수는 : " + postMapper.findAll().size() + "개입니다.");
 	    }
 		
 }
