f0bf0020f90182493cddbc635856d3a0687ae748
he22ra <hhraleeee@gamil.com>
2023-05-18 15:24:54 +0900

    게시판 테스트 : update 완료

--- src/test/java/com/blog/main/PostMapperTest.java
+++ src/test/java/com/blog/main/PostMapperTest.java
@@ -34,9 +34,31 @@ public class PostMapperTest {
 //		}
 		
 		// 게시물 조회 테스트
+//		@Test
+//	    void findById() {
+//			PostResponse post = postMapper.findById(6L);
+//	        try {
+//	            String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
+//	            System.out.println(postJson);
+//
+//	        } catch (JsonProcessingException e) {
+//	            throw new RuntimeException(e);
+//	        }
+//	    }
+		
 		@Test
-	    void findById() {
-			PostResponse post = postMapper.findById(6L);
+	    void update() {
+	        // 1. 게시글 수정
+	        PostRequest params = new PostRequest();
+	        params.setId(1L);
+	        params.setTitle("1번 게시글 제목 수정합니다.");
+	        params.setContents("1번 게시글 내용 수정합니다.");
+	        params.setWriter("수정테스터");
+	        params.setNoticeYn(1);
+	        postMapper.update(params);
+
+	        // 2. 게시글 상세정보 조회
+	        PostResponse post = postMapper.findById(1L);
 	        try {
 	            String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
 	            System.out.println(postJson);
