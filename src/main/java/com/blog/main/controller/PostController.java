d9f2703ca0034c62c3707d1cde16181e22c02e03
he22ra <hhraleeee@gamil.com>
2023-05-19 17:29:54 +0900

    Thymeleaf : 게시판 write, save, list 페이지 구현

--- src/main/java/com/blog/main/controller/PostController.java
+++ src/main/java/com/blog/main/controller/PostController.java
@@ -1,9 +1,15 @@
 package com.blog.main.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.blog.main.post.PostRequest;
+import com.blog.main.post.PostResponse;
 import com.blog.main.service.PostService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,17 +20,29 @@ public class PostController {
 
 	private final PostService postService;
 	
+	// 게시글 리스트 페이지
+	@GetMapping("/post/list.do")
+	public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+        return "post/list";
+    }
+	
+	// 게시글 작성 페이지
 	@GetMapping("/post/write.do")
-	public String openPostWrite(Model model) {
-		
-		String title = "제목",
-               content = "내용",
-               writer = "홍길동";
-
-        model.addAttribute("t", title);
-        model.addAttribute("c", content);
-        model.addAttribute("w", writer);
-	        
+	public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model) {
+		// 글 수정시
+		if (id != null) {
+			PostResponse post = postService.findPostById(id);
+			model.addAttribute("post", post);
+		}
 		return "post/write";
 	}
+	
+	// 신규 게시글 생성
+    @PostMapping("/post/save.do")
+    public String savePost(final PostRequest params) {
+        postService.savePost(params);
+        return "redirect:/post/list.do";
+    }
 }
