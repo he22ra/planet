2f4c66c35af7dc8e14b77a18c216051d8563246a
he22ra <hhraleeee@gamil.com>
2023-05-22 14:05:25 +0900

    jdbc log 옵션 설정 : build.gradle, logback-spring.xml

---
+++ src/main/java/com/blog/main/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.blog.main.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.blog.main.interceptor.LoggerInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor())
+				.excludePathPatterns("/css/**", "/images/**", "/js/**");
+	}
+}
