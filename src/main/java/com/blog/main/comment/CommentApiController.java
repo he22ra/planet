a88737d0f8df8d72962f352ff9a5d6dd7815eb62
he22ra <hhraleeee@gmail.com>
2023-05-24 17:25:52 +0900

    REST API 댓글 : 수정 modal

--- src/main/java/com/blog/main/comment/CommentApiController.java
+++ src/main/java/com/blog/main/comment/CommentApiController.java
@@ -3,6 +3,7 @@ package com.blog.main.comment;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,5 +33,18 @@ public class CommentApiController {
     @GetMapping("/post/{postId}/comments")
     public List<CommentResponse> findAllComment(@PathVariable final Long postId) {
         return commentService.findAllComment(postId);
+    }
+    
+    // 댓글 상세정보 조회
+    @GetMapping("/post/{postId}/comments/{id}")
+    public CommentResponse findCommentById(@PathVariable final Long postId, @PathVariable final Long id) {
+        return commentService.findCommentById(id);
+    }
+    
+    // 기존 댓글 수정
+    @PatchMapping("/post/{postId}/comments/{id}")
+    public CommentResponse updateComment(@PathVariable final Long postId, @PathVariable final Long id, @RequestBody final CommentRequest params) {
+        commentService.updateComment(params);
+        return commentService.findCommentById(id);
     }
 }
